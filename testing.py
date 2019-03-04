e0b1772ba74e8ce007b7ab492a1241add727a809
nhoogendam <nhoogendam@gmail.com>
2019-03-03 22:34:49 -0600

    Created a GetAverages function that creates an array of average stock prices for every minute of that specified day.

--- testing.py
+++ testing.py
@@ -9,9 +9,23 @@ model.add(keras.layers.Dense(64, activation='relu'))
 model.add(keras.layers.Dense(64, activation='relu'))
 model.add(keras.layers.Dense(1, activation='softmax'))
 
+#DESC:  returns an array of all the minute
+#       averages throughout the day
+#      -stk -> stock abbreiviation
+#      -day -> day of minute stock data
+def GetAverages(stk, day):
+    df = get_historical_intraday(stk, day)
+    averages = []
+    for minAver in df:
+        averages.append(minAver.get("average"))
+    return averages
 
 
 
+startDate = datetime(2018, 1, 27)
+endDate = datetime(2019, 3, 1)
 
-stk = Stock('TSLA')
-print(stk.get_price())
+stk = 'AMZN'
+print(GetAverages(stk, endDate))
+
+
