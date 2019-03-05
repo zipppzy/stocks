48fdf59d92cd69f03b73aa838748e6e9793bc211
zipppzy <zipppzy939@gmail.com>
2019-03-04 18:41:11 -0600

    added function GetMultipleIntradayData renamed GetAverageFires to GetIntradayPrices

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
-from datetime import datetime
-from iexfinance.stocks import Stock, get_historical_data, get_historical_intraday  #https://addisonlynch.github.io/iexfinance/devel/index.html
+from datetime import datetime, timedelta
+from iexfinance.stocks import Stock, get_historical_data, get_historical_intraday # https://addisonlynch.github.io/iexfinance/devel/index.html
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -9,24 +9,41 @@ model.add(keras.layers.Dense(64, activation='relu'))
 model.add(keras.layers.Dense(64, activation='relu'))
 model.add(keras.layers.Dense(1, activation='softmax'))
 
-#DESC:  returns an array of all the minute
-#       averages throughout the day
+# DESC:  returns an array of all the minute
 #      -stk -> stock abbreiviation
 #      -key -> field of data i.e. average, high, low
 #      -day -> day of minute stock data
-def GetAverages(stk, key, day):
+def GetIntradayPrices(stk, key, day):
     df = get_historical_intraday(stk, day)
     data = []
     for minAver in df:
         data.append(minAver.get(key))
     return data
 
+# DESC:  returns an array of all the minutes in between startDate and endDate
+#      -stk -> stock abbreiviation
+#      -key -> field of data i.e. average, high, low
+#      -startDate -> first day to take stock data from
+#      -endDate -> last day to take stock data from
+def GetMultipleIntradayData(stk, key, startDate, endDate):
+    dateIncrement = startDate
+    dayDelta = timedelta(days=1)
+    data =[]
+    while dateIncrement != endDate:
+        data += GetIntradayPrices(stk, key, dateIncrement)
+        dateIncrement = dateIncrement+dayDelta
+    return data
 
 
-startDate = datetime(2018, 1, 27)
+
+startDate = datetime(2019, 1, 1)
 endDate = datetime(2019, 3, 1)
 
 stk = 'AMZN'
-print(GetAverages(stk,"average", endDate))
+# print(GetAverages(stk, "average", endDate))
+# print(len(GetAverages(stk, "average", endDate)))
 
+data = GetMultipleIntradayData(stk, "average", startDate, endDate)
+print(data)
+print(len(data))
 
